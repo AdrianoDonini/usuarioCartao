f5d3373fce54a7d5e1e1f8862454c6bc29683566
AdrianoDonini <2003donini@gmail.com>
2024-04-19 20:31:54 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import  { Router} from 'express';
+
+import { CreateUserController } from './controllers/user/CreateUserController';
+
+const router = Router();
+router.post('/user', new CreateUserController().handle);
+export {router};
---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,4 @@
+import prismaClient from "../../prisma";
+import { compare } from "bcryptjs";
+import {sign} from 'jsonwebtoken';
+
